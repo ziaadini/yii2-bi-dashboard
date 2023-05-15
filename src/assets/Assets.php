7c0d78ff94a088745bc7d6ad8414fab96f6df5fc
baghaee <baghaee.sr@gmail.com>
2023-05-15 15:22:54 +0330

    edit views

--- src/assets/Assets.php
+++ src/assets/Assets.php
@@ -2,7 +2,21 @@
 
 namespace sadi01\bidashboard\assets;
 
-class Assets
-{
+use yii\web\AssetBundle;
 
+class AppAsset extends AssetBundle
+{
+    public $sourcePath = '@sadi01/bidashboard/assets/theme';
+
+
+    public $css = [
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap5\BootstrapAsset',
+        'yii\widgets\ActiveFormAsset',
+        'yii\validators\ValidationAsset',
+    ];
 }
--- src/views/layouts/bid_main.php
+++ src/views/layouts/bid_main.php
@@ -1,6 +1,13 @@
 <?php
 use Yii;
 use yii\bootstrap5\Html;
+
+/** @var \yii\web\View $this */
+/** @var string $content */
+
+//list(,$url) = Yii::$app->assetManager->publish('@sadi01/bidashboard/assets');
+//$this->registerCssFile($url.'/theme/vendors/bundle.css');
+
 ?>
 <?php $this->beginPage() ?>
     <!DOCTYPE html>
@@ -10,20 +17,21 @@ use yii\bootstrap5\Html;
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <?php $this->registerCsrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta http-equiv="Pragma" content="no-cache" />
+        <meta http-equiv="Expires" content="0" />
     </head>
     <body class="d-flex flex-column h-100">
     <?php $this->beginBody() ?>
             <?= $content ?>
     <?php $this->endBody() ?>
     </body>
-
     <?php if (isset($this->blocks['javascript'])): ?>
         <?= $this->blocks['javascript'] ?>
     <?php else: ?>
         <script></script>
     <?php endif; ?>
-
-
     </html>
 <?php $this->endPage();
 
