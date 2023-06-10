6b282ce5f0d2135a72d1810a73d1237e4b8b1e10
baghaee <baghaee.sr@gmail.com>
2023-06-10 10:36:11 +0330

    calcutlate widget result

--- src/controllers/ReportWidgetController.php
+++ src/controllers/ReportWidgetController.php
@@ -2,6 +2,7 @@
 
 namespace sadi01\bidashboard\controllers;
 
+use sadi01\bidashboard\models\ReportWidgetResult;
 use Yii;
 use sadi01\bidashboard\models\ReportWidget;
 use sadi01\bidashboard\models\ReportWidgetSearch;
@@ -64,13 +65,18 @@ class ReportWidgetController extends Controller
         $params = $model->add_on['params'];
 
 //---- create url search
-        $modelRoute = "/".$model->search_route."?";
+        $modelRoute = "/" . $model->search_route . "?";
         $modalRouteParams = "";
-        foreach ($params as $key => $param){
-            $modalRouteParams .= $model->search_model_form_name."[".$key."]=".$param."&";
+        foreach ($params as $key => $param) {
+            $modalRouteParams .= $model->search_model_form_name . "[" . $key . "]=" . $param . "&";
         }
         $modelRoute .= $modalRouteParams;
 //---- end
+
+        $runWidget = ReportWidgetResult::findOne(['widget_id' => $id]);
+        if (!$runWidget) {
+            $runWidget = $this->actionRunWidget($id, null, null);
+        }
 
         return $this->render('view', [
             'model' => $model,
@@ -89,12 +95,12 @@ class ReportWidgetController extends Controller
         $model->loadDefaultValues();
 
         $request = $this->request->get();
-        $searchModelClass = $this->requestGet($request,'searchModelClass');
-        $searchModelMethod = $this->requestGet($request,'searchModelMethod');
-        $searchModelRunResultView = $this->requestGet($request,'searchModelRunResultView');
-        $search_route = $this->requestGet($request,'search_route');
-        $search_model_form_name = $this->requestGet($request,'search_model_form_name');
-        $queryParams = $this->requestGet($request,'queryParams');
+        $searchModelClass = $this->requestGet($request, 'searchModelClass');
+        $searchModelMethod = $this->requestGet($request, 'searchModelMethod');
+        $searchModelRunResultView = $this->requestGet($request, 'searchModelRunResultView');
+        $search_route = $this->requestGet($request, 'search_route');
+        $search_model_form_name = $this->requestGet($request, 'search_model_form_name');
+        $queryParams = $this->requestGet($request, 'queryParams');
 
         if ($this->request->isPost) {
             if ($model->load($this->request->post()) && $model->validate()) {
@@ -103,7 +109,7 @@ class ReportWidgetController extends Controller
                     'success' => true,
                     'msg' => Yii::t('biDashboard', 'Saved successfully'),
                 ]);
-            }else{
+            } else {
                 return $this->asJson([
                     'success' => false,
                     'msg' => Yii::t('biDashboard', 'There was a problem saving'),
@@ -173,8 +179,8 @@ class ReportWidgetController extends Controller
         }
         throw new NotFoundHttpException(Yii::t('biDashboard', 'The requested page does not exist.'));
     }
-    
-    public function actionOpenModal(){
+    public function actionOpenModal()
+    {
         $searchModel = new ReportWidgetSearch();
         $dataProvider = $searchModel->search($this->request->queryParams);
 
@@ -184,29 +190,105 @@ class ReportWidgetController extends Controller
         ]);
     }
 
-    public function requestGet($request,$key){
-        if (key_exists($key,$request)){
+    public function requestGet($request, $key)
+    {
+        if (key_exists($key, $request)) {
             return $request[$key];
         }
         return null;
     }
 
     /**
-     * @param int $id ID
+     * @param $id
+     * @param $start_range
+     * @param $end_rage
+     * @return mixed
+     * @throws \Exception
      */
-    public function actionRunWidget($id){
-        $model = $this->findModel($id);
+    public function actionRunWidget($id, $start_range=null, $end_range=null)
+    {
+        $widget = $this->findModel($id);
+        $nowDate = new \DateTime('UTC');
+        $pDate = \Yii::$app->pdate;
+        $jNowDate = $pDate->jgetdate();
+        $CNTmonthDay = (int)$jNowDate['mon'] <= 6 ? 31 : 30;
 
+        $j_year_start_range = $jNowDate['year'];
+        $j_month_start_range = $jNowDate['mon'];
+        if ($widget->range_type == $widget::RANGE_TYPE_DAILY) {
+            if (!$start_range) {
+                $start_range_datetime = new \DateTime($pDate->jalali_to_gregorian($j_year_start_range, $j_month_start_range, '1', '/'));
+            } else {
+                $start_range_datetime = new \DateTime($pDate->jalali_to_gregorian($start_range['year'], $start_range['month'], $start_range['day'], '/'));
+            }
+            if (!$end_range) {
+                $j_day_range = $jNowDate['mday'];
+                $end_range_datetime = new \DateTime($pDate->jalali_to_gregorian($j_year_start_range, $j_month_start_range, $j_day_range, '/'));
+            } else {
+                $end_range_datetime = new \DateTime($pDate->jalali_to_gregorian($end_range['year'], $end_range['month'], $end_range['day'], '/'));
+            }
+        } else {
+            if (!$start_range) {
+                $start_range_datetime = new \DateTime($pDate->jalali_to_gregorian($j_year_start_range, '1', '1', '/'));
+            } else {
+                $start_range_datetime = new \DateTime($pDate->jalali_to_gregorian($start_range['year'], $start_range['month'], '1', '/'));
+            }
+            if (!$end_range) {
+                $end_range_datetime = new \DateTime($pDate->jalali_to_gregorian($j_year_start_range, $j_month_start_range, $CNTmonthDay, '/'));
+            } else {
+                $end_range_datetime = new \DateTime($pDate->jalali_to_gregorian($end_range['year'], $end_range['month'], $CNTmonthDay, '/'));
+            }
+        }
+
+        $modelQueryResults = [];
+        if ($widget->range_type == $widget::RANGE_TYPE_DAILY) {
+            for ($i = 1; $i <= $CNTmonthDay; $i++) {
+                $startDate = new \DateTime($pDate->jalali_to_gregorian($j_year_start_range, $j_month_start_range, $i, '/'));
+                $endDate = $startDate->setTime(23, 23, 59);
+                $modelQueryResults[$i] = $this->findSearchModelWidget($widget, $startDate, $endDate, 'm')[0];
+                $modelQueryResults[$i]['title'] = $i;
+            }
+        } else {
+            $months = [1 => 'فروردین', 2 => 'اردیبشهت', 3 => 'خرداد', 4 => 'تیر', 5 => 'مرداد', 6 => 'شهریور', 7 => 'مهر', 8 => 'آبان', 9 => 'آذر', 10 => 'دی', 11 => 'بهمن', 12 => 'اسفند'];
+            foreach ($months as $key => $month) {
+                $startDate = new \DateTime($pDate->jalali_to_gregorian($j_year_start_range, $key, '1', '/'));
+                $endDate = new \DateTime($pDate->jalali_to_gregorian($j_year_start_range, $key, $key <= 6 ? '31' : '30', '/'));
+
+                $modelQueryResults[$key] = $this->findSearchModelWidget($widget, $startDate, $endDate, 'm')[0];
+                $modelQueryResults[$key]['title'] = $month;
+            }
+        }
+
+
+        // -- create Report Widget Result
+        $reportWidgetResult = new ReportWidgetResult();
+        $reportWidgetResult->widget_id = $id;
+        $reportWidgetResult->status = 10;
+        $reportWidgetResult->start_range = $start_range_datetime->getTimestamp();
+        $reportWidgetResult->end_range = $end_range_datetime->getTimestamp();
+        $reportWidgetResult->run_action = Yii::$app->controller->action->id;
+        $reportWidgetResult->run_controller = Yii::$app->controller->id;
+        $reportWidgetResult->result = $modelQueryResults;
+        $reportWidgetResult->save();
+
+        return [
+            'success' => true,
+            'msg' => Yii::t('biDashboard', 'Saved successfully'),
+        ];
+    }
+
+    public function findSearchModelWidget($model, $startDate, $endDate)
+    {
         $params = $model->add_on['params'];
         $searchModel = new ($model->search_model_class);
-        $methodExists = method_exists($searchModel,'search');
-        if ($methodExists){
-            $dataProvider = $searchModel->{$model->search_model_method}($params);
-        }else{
+        $methodExists = method_exists($searchModel, 'search');
+        if ($methodExists) {
+            $dataProvider = $searchModel->{$model->search_model_method}($params, $startDate, $endDate);
+        } else {
             //TODO: error
         }
-        $models = array_values($dataProvider->getModels());
+        $modelQueryResults = array_values($dataProvider->query->asArray()->all());
 
-        dd('models',$models);
+        return $modelQueryResults;
     }
 }
