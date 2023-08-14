4ebdb2cce8c04915af096f8de1e0b3b4577c4db3
baghaee <baghaee.sr@gmail.com>
2023-08-14 13:30:14 +0330

    margin

--- src/widgets/views/ReportModalWidget.php
+++ src/widgets/views/ReportModalWidget.php
@@ -46,7 +46,8 @@ $this->title = 'Bi dashboard widget';
             'queryParams' => json_encode($queryParams),
             'output_column' => json_encode($outputColumn ?? []),
         ]),
-        'data-handle-form-submit' => 1
+        'data-handle-form-submit' => 1,
+        'class' => 'm-1',
     ]) ?>
 
 <?= Html::a(Html::tag('span', 'لیست ویجت‌ها', ['class' => ['btn btn-info']]), "javascript:void(0)",
@@ -62,5 +63,6 @@ $this->title = 'Bi dashboard widget';
             'ReportWidgetSearch[search_model_class]' => $searchModel::class,
             'ReportWidgetSearch[search_model_method]' => $searchModelMethod,
         ]),
-        'data-handle-form-submit' => 0
+        'data-handle-form-submit' => 0,
+        'class' => 'm-1',
     ]) ?>
