734c4c567dd49d659d9fe1c955e23dda4c55b1a1
Mohsen Saeidi <mohsen.saeidi1994@gmail.com>
2024-04-29 16:34:41 +0330

    fix search widget & link to dashboard on delete widget

--- src/views/report-widget/index.php
+++ src/views/report-widget/index.php
@@ -15,45 +15,41 @@ use yii\widgets\Pjax;
 $this->title = Yii::t('biDashboard', 'Report Widgets');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<?php Pjax::begin(['id' => 'p-jax-report-widget', 'enablePushState' => false]); ?>
-    <div class="page-content container-fluid text-left">
-        <div class="work-report-index card">
-            <div class="panel-group m-bot20" id="accordion">
-                <div class="card-header d-flex justify-content-between">
-                    <h4 class="panel-title">
-                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
-                           href="#collapseSearch" aria-expanded="false">
-                            <i class="fa fa-search"></i> جستجو
-                        </a>
-                    </h4>
+
+<div class="page-content container-fluid text-left pt-5" id="main-wrapper">
+    <?php Pjax::begin(['id' => 'p-jax-report-widget', 'enablePushState' => false]); ?>
+    <div class="work-report-index card">
+        <div class="panel-group m-bot20" id="accordion">
+            <div class="card-header d-flex justify-content-between">
+                <div class="d-flex align-items-center font-16 font-bold">
+                    <span><?= Yii::t('biDashboard', 'Widget List') ?></span>
                 </div>
-                <div class="card-body">
-                    <div id="collapseSearch" class="panel-collapse collapse" aria-expanded="false">
-                        <?php echo $this->render('_search', ['model' => $searchModel]); ?>
-                    </div>
-                    <?= GridView::widget([
-                        'dataProvider' => $dataProvider,
-                        'columns' => [
-                            ['class' => 'yii\grid\SerialColumn'],
-                            'id',
-                            'title',
-                            'description',
-                            [
-                                'attribute' => 'range_type',
-                                'value' => function (ReportWidget $model) {
-                                    return ReportWidget::itemAlias('RangeTypes', $model->range_type);
-                                },
-                            ],
-                            'search_model_class',
-                            'search_model_method',
-                            [
-                                'class' => ActionColumn::class,
-                                'urlCreator' => function ($action, ReportWidget $model, $key, $index, $column) {
-                                    return Url::toRoute([$action, 'id' => $model->id]);
-                                },
-                                'template' => '{view} {delete}',
-                                'buttons' => [
-                                    'delete' => function ($url, ReportWidget $model, $key) {
+                <?= $this->render('_search', ['model' => $searchModel]); ?>
+            </div>
+            <div class="card-body page-content container-fluid text-left">
+                <?= GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    'columns' => [
+                        ['class' => 'yii\grid\SerialColumn'],
+                        'id',
+                        'title',
+                        'description',
+                        [
+                            'attribute' => 'range_type',
+                            'value' => function (ReportWidget $model) {
+                                return ReportWidget::itemAlias('RangeTypes', $model->range_type);
+                            },
+                        ],
+                        'search_model_class',
+                        'search_model_method',
+                        [
+                            'class' => ActionColumn::class,
+                            'urlCreator' => function ($action, ReportWidget $model, $key, $index, $column) {
+                                return Url::toRoute([$action, 'id' => $model->id]);
+                            },
+                            'template' => '{view} {delete}',
+                            'buttons' => [
+                                'delete' => function ($url, ReportWidget $model, $key) {
                                     return Html::a('<i class="fas fa-trash-alt"></i>', "javascript:void(0)",
                                         [
                                             'data-pjax' => '0',
@@ -66,29 +62,29 @@ $this->params['breadcrumbs'][] = $this->title;
                                             'data-handle-form-submit' => 1,
                                             'data-reload-pjax-container' => 'p-jax-report-widget'
                                         ]);
-                                    },
-                                    'view' => function ($url, ReportWidget $model, $key) {
-                                        return Html::a('<i class="fa fa-eye"></i>', "javascript:void(0)",
-                                            [
-                                                'data-pjax' => '0',
-                                                'class' => "btn text-info p-0",
-                                                'data-size' => 'modal-xl',
-                                                'data-title' => Yii::t('biDashboard', 'view'),
-                                                'data-toggle' => 'modal',
-                                                'data-target' => '#modal-pjax-bi',
-                                                'data-url' => Url::to(['report-widget/view', 'id' => $model->id]),
-                                                'data-handle-form-submit' => 1,
-                                                'data-reload-pjax-container' => 'p-jax-report-widget',
-                                            ]
-                                        );
-                                    },
-                                ],
+                                },
+                                'view' => function ($url, ReportWidget $model, $key) {
+                                    return Html::a('<i class="fa fa-eye"></i>', "javascript:void(0)",
+                                        [
+                                            'data-pjax' => '0',
+                                            'class' => "btn text-info p-0",
+                                            'data-size' => 'modal-xl',
+                                            'data-title' => Yii::t('biDashboard', 'view'),
+                                            'data-toggle' => 'modal',
+                                            'data-target' => '#modal-pjax-bi',
+                                            'data-url' => Url::to(['report-widget/view', 'id' => $model->id]),
+                                            'data-handle-form-submit' => 1,
+                                            'data-reload-pjax-container' => 'p-jax-report-widget',
+                                        ]
+                                    );
+                                },
                             ],
                         ],
-                    ]); ?>
-
-                </div>
+                    ],
+                    'summary' => "",
+                ]); ?>
             </div>
         </div>
     </div>
-<?php Pjax::end(); ?>
+    <?php Pjax::end(); ?>
+</div>
